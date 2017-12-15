c6396d5a6437f913b19467f46c945f5ad9b8cc97
dar7c0d3 <34436785+dar7c0d3@users.noreply.github.com>
2017-12-15 19:07:38 +0530

    Add files via upload

---
+++ tic.py
@@ -0,0 +1,96 @@
+import random
+from __future__ import print_function
+from IPython.display import clear_output
+def display_board(board):
+	clear_output()
+	print('   |   |')
+	print(' '+ board[7] + ' | ' + board[8] + ' | ' + board[9] )
+	print('   |   |  ')
+	print('-----------')
+	print('   |   |')
+	print(' '+ board[4] + ' | ' + board[5] + ' | ' + board[6] )
+	print('   |   |  ')
+	print('-----------')
+	print('   |   |')
+	print(' '+ board[1] + ' | ' + board[2] + ' | ' + board[3] )
+	print('   |   |  ')
+def player_input():
+	marker = ''
+	while not (marker == 'X' or marker == 'O'):
+		marker = raw_input('Do you want to be X or O ? ').upper()
+	if marker == 'X':
+		return ('X','O')
+	else:
+		return ('O','X')
+def place_marker(board,marker,position):
+	board[position] = marker
+def win_check(board,mark):
+	return (( board[7] == mark and board[8] == mark and board[9] == mark) or
+			( board[4] == mark and board[5] == mark and board[6] == mark) or
+			( board[1] == mark and board[2] == mark and board[3] == mark) or
+			( board[7] == mark and board[4] == mark and board[1] == mark) or
+			( board[8] == mark and board[5] == mark and board[2] == mark) or
+			( board[9] == mark and board[6] == mark and board[3] == mark) or
+			( board[1] == mark and board[5] == mark and board[9] == mark) or
+			( board[7] == mark and board[5] == mark and board[3] == mark))
+def choose_first():
+	if random.randint(0,1) == 0:
+		return 'Player 1'
+	else:
+		return 'Player 2'
+def space_check(board,position):
+	return board[position] == ' '
+def full_board_check(board):
+	for i in range(1,10):
+		if space_check(board,i):
+			return False
+	return True
+def player_choice(board):
+	position = ' '
+	while position not in ' 1 2 3 4 5 6 7 8 9'.split() or not space_check(board,int(position)):
+		position = raw_input('Choose your next position: (1-9)')
+	return int(position)
+def replay():
+	return raw_input('Do you want to play again ? Enter Yes or No ').upper().startswith('Y')
+
+#Game Logic
+print('Welcome to Tic Tac Toe!!')
+while True:
+	playBoard = [' ']*10
+	player1_marker, player2_marker = player_input()
+	turn = choose_first()
+	print(turn + ' will go first!')
+	game_on = True
+	while game_on:
+		if turn == 'Player 1':
+			display_board(playBoard)
+			position = player_choice(playBoard)
+			place_marker(playBoard,player1_marker,position)
+			if win_check(playBoard,player1_marker):
+				display_board(playBoard)
+				print('Congrts!! Player 1 won the game')
+				game_on = False
+			else:
+				if full_board_check(playBoard):
+					display_board(playBoard)
+					print('This game is a draw')
+					break
+				else:
+					turn = 'Player 2'
+		else:
+			display_board(playBoard)
+			position = player_choice(playBoard)
+			place_marker(playBoard,player2_marker,position)
+			if win_check(playBoard,player2_marker):
+				display_board(playBoard)
+				print('Congrts!! Player 2 won the game')
+				game_on = False
+			else:
+				if full_board_check(playBoard):
+					display_board(playBoard)
+					print('This game is a draw')
+					break
+				else:
+					turn = 'Player 1'
+	if not replay():
+		break
